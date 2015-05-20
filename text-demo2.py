8e8d596dcbaa64fe15092e25ad50a32a99097e0c
Jakub Hejhal <kubic71@gmail.com>
2015-05-20 16:26:17 +0200

    - New game - RUSH, - pyvo-sraz text demo made, show_text function for even easier text displaying

--- text-demo2.py
+++ text-demo2.py
@@ -1,4 +1,4 @@
-import svetelny_panel, colors
+import svetelny_panel, colors, time
 from textHandler import *
 from matrixHandler import *
 
@@ -11,7 +11,7 @@ textHandler = TextHandler()
 
 textToView = raw_input("Text to view:")
 
-text, textwidth = textHandler.make_text(textToView, x_shift=16, y_shift=0, color=colors.PINK)     #Text is shifted 16 pixels horizonataly to right at the very beggining
+text, textwidth = textHandler.make_text(textToView, x_shift=16, y_shift=1, color=colors.PURPLE)     #Text is shifted 16 pixels horizonataly to right at the very beggining
 
 engine = MatrixEngine(text) #Creates Engine object with text from 'textToView' variable
 
@@ -22,7 +22,8 @@ while True:
     engine.shift_left()
     matrix = engine.get_matrix(cycle_x=True, cycle_size_x = textwidth+16)   #Moving text
     svetelny_panel.set_panel_memory_from_matrix(matrix)
-    
+
+    time.sleep(0.05)
     #engine.print_matrix()  #Prints matrix to console
 
 
